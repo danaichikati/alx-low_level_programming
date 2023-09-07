247b68c0388ecb942593e59ba4bc0170b59fd884
danai chikati <danaichikati@gmail.com>
2023-09-07 15:57:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	printf("\"With proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
